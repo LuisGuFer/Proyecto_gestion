c90b0fc77f62403f9a61126fe19494727d6a549b
adalmar <aherdoizam@unemi.edu.com>
2026-07-05 12:10:43 -0500

    despliege definitivo vercel

--- server.js
+++ server.js
@@ -1,13 +1,18 @@
 const express = require("express");
+const path = require("path");
 
 const app = express();
 
 const PORT = process.env.PORT || 3000;
 
+// Servir archivos estáticos
+app.use(express.static(path.join(__dirname, "public")));
+
+// Ruta principal
 app.get("/", (req, res) => {
-  res.send("API funcionando en Vercel");
+    res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
 app.listen(PORT, () => {
-  console.log(`Servidor en puerto ${PORT}`);
+    console.log(`Servidor ejecutándose en el puerto ${PORT}`);
 });
